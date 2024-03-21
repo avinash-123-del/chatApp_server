e2d6bae74ea0166c9f54059030aa6e6b8fb00268
avinash-123-del <avichandraker@gmail.com>
2024-03-21 07:57:05 +0530

    1

--- controllers/ct_message.js
+++ controllers/ct_message.js
@@ -1,10 +1,37 @@
-import { messageSchema } from "../models/tbl_chatMessage";
+import { messageSchema } from "../models/tbl_chatMessage.js";
 
 const addMessage = async (req,res) => { 
-          const result = await messageSchema.create(message);
-    return result;
+    const {chatId , senderId , text} = req.body;
+
+    try {
+        const createMessage  = await messageSchema.create({chatId , senderId , text})
+    
+        await createMessage.save();
+    
+        return res.status(201).json({message : "message send successfully "});
+
+    } catch (error) {
+        return res.status(500).json({message : "internal server error"});
+    }
+
+}
+
+const getMessages = async (req,res) => {
+
+    const {chatId} = req.params.chatId
+    try {
+        
+        const findChats  = await messageSchema.find({chatId})
+
+        return res.status(201).json({message : "all chats" , findChats})
+    } catch (error) {
+        return res.status(501).send("internal server error ")
+    }
+
+
+
 }
 
 
 
-export {addMessage}
+export {addMessage , getMessages}
--- routes/rt_message.js
+++ routes/rt_message.js
@@ -1,10 +1,11 @@
 import express from 'express';
+import { addMessage, getMessages } from '../controllers/ct_message.js';
 
 const router = express.Router();
 
-// router.use(express.json())
+router.use(express.json())
 
-// router.route('/').post(addMessage)
-// router.route('/:chatId').post(getMessages)
+router.route('/').post(addMessage)
+router.route('/:chatId').post(getMessages)
 
 export default router;
