edee9c86e7d85f1f44f769e69046d6d38973bae5
avinash-123-del <avichandraker@gmail.com>
2024-03-26 22:09:44 +0530

    3

--- index.js
+++ index.js
@@ -5,8 +5,22 @@ import mongooseConnect from "./connection/connectionDB.js";
 import user from "./routes/rt_user.js"
 import chat from "./routes/rt_chat.js" 
 import message from "./routes/rt_message.js"
+import {Server} from 'socket.io'
+import {createServer} from 'http'
 
 const app = express();
+const server = createServer(app)
+const io = new Server(server , {
+    cors : {
+        origin : "http://localhost:3000",
+        methods : ["GET" , "POST"],
+        credentials : true
+    }
+})
+
+io.on('connection', (socket) => {  
+   console.log("user connectted" , socket.id)
+ })
 
 await mongooseConnect;
 
@@ -23,4 +37,4 @@ app.get("/", (req, res) => {
 })
 
 
-app.listen(process.env.PORT, () => { console.log(`server started on ${process.env.PORT}`); })
+server.listen(process.env.PORT, () => { console.log(`server started on ${process.env.PORT}`); })
