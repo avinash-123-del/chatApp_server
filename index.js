db31789d4f3c9107ecb582c5c37687123fa5a93f
Avinash Chandraker <61513521+avinash-123-del@users.noreply.github.com>
2024-03-30 17:17:15 +0530

    1

--- index.js
+++ index.js
@@ -3,24 +3,35 @@ import cors from "cors";
 import dotenv from "dotenv";
 import mongooseConnect from "./connection/connectionDB.js";
 import user from "./routes/rt_user.js"
-import chat from "./routes/rt_chat.js" 
+import chat from "./routes/rt_chat.js"
 import message from "./routes/rt_message.js"
-import {Server} from 'socket.io'
-import {createServer} from 'http'
+import { Server } from 'socket.io'
+import { createServer } from 'http'
 
 const app = express();
 const server = createServer(app)
-const io = new Server(server , {
-    cors : {
-        origin : "http://localhost:3000",
-        methods : ["GET" , "POST"],
-        credentials : true
-    }
+const io = new Server(server, {
+          cors: {
+                    origin: "http://localhost:3000",
+                    methods: ["GET", "POST"],
+                    credentials: true
+          }
 })
 
-io.on('connection', (socket) => {  
-   console.log("user connectted" , socket.id)
- })
+io.on('connection', (socket) => {
+          console.log("user connectted", socket.id)
+
+
+          socket.on('disconnect', () => {
+                    console.log("user disconnected", socket.id)
+          })
+
+          socket.on('send_message', (data) => {
+                    console.log("message sent", data)
+                    io.emit("get_message", data)
+          })
+
+})
 
 await mongooseConnect;
 
